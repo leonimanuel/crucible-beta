3cb74190a70f6b254b02123c58106f0ccc623d51
LIManuel <leonmalisov@gmail.com>
2020-06-10 21:37:58 -0400

    expanded fact create method to handle fact movement across topics. Haven't tested

--- crucible-api/app/controllers/facts_controller.rb
+++ crucible-api/app/controllers/facts_controller.rb
@@ -1,15 +1,22 @@
 class FactsController < ApplicationController
 	def create
-		# binding.pry
-		fact = Fact.new(content: params[:selected_text], url: params[:selection_url])
-		if fact.valid?
-			# binding.pry
-			fact.save
-			user = @current_user
-			user.topics.find_by(name: "New Facts").facts << fact 
-			render json: FactSerializer.new(fact).to_serialized_json
-		else				
-			render json: {error: "could not save fact"}
+		user = @current_user
+
+		if params[:origin_topic_name]
+			fact = Fact.find(params[:fact_id])
+			user.topics.find_by(name: params[:origin_topic_name]).facts.delete(fact)
+			user.topics.find_by(name: params[:destination_topic_name]).facts << fact
+			binding.pry
+		else
+			fact = Fact.new(content: params[:selected_text], url: params[:selection_url])
+			if fact.valid?
+				# binding.pry
+				fact.save
+				user.topics.find_by(name: "New Facts").facts << fact 
+				render json: FactSerializer.new(fact).to_serialized_json
+			else				
+				render json: {error: "could not save fact"}
+			end			
 		end
 	end
 end
