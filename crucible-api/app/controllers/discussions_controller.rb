f2a284e83570b60329e2b341636089be2a63d80a
LIManuel <leonmalisov@gmail.com>
2020-06-24 12:01:28 -0400

    updated create action for discussions

--- crucible-api/app/controllers/discussions_controller.rb
+++ crucible-api/app/controllers/discussions_controller.rb
@@ -8,27 +8,8 @@ require 'json'
 class DiscussionsController < ApplicationController
 	def create
 		user = @current_user
-		article_url = params[:article_url]
-
-		# response = Faraday.get "https://extractorapi.com/api/v1/extractor/?apikey=#{ENV["EXTRACTOR_KEY"]}&url=#{article_url}&fields=clean_html"
-		# json_response = JSON.parse(response.body)
-
-# curl --verbose \
-#        --user '35f5808325ea48adb080ab0f82a5c431':'' \
-#        --header 'Content-Type: application/json' \
-#        --data '[{"url": "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html", "pageType": "product"}]' \
-#        --max-time 605 \
-#        --compressed \
-#        https://autoextract.scrapinghub.com/v1/extract
-
-# 		c = Curl::Easy.perform("https://autoextract.scrapinghub.com/v1/extract") do |curl|
-# 		  curl.verbose = true
-# 		  curl.userpwd = "35f5808325ea48adb080ab0f82a5c431"
-# 		  curl.headers["Content-Type"] = "application/json"
-# 		  curl.data = '[{"url": "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html", "pageType": "product"}]'
-# 		end
-
-# 		c.perform
+		article_url = params[:articleURL]
+		binding.pry
 
 		uri = URI.parse("https://autoextract.scrapinghub.com/v1/extract")
 		request = Net::HTTP::Post.new(uri)
@@ -36,7 +17,7 @@ class DiscussionsController < ApplicationController
 		request.content_type = "application/json"
 		request.body = JSON.dump([
 		  {
-		    "url" => "https://www.nytimes.com/2020/06/20/nyregion/trump-geoffrey-berman-fired-sdny.html",
+		    "url" => article_url,
 		    "pageType" => "article",
 		    "articleBodyRaw" => false
 		  }
@@ -52,15 +33,13 @@ class DiscussionsController < ApplicationController
 
 		boi = JSON.parse(response.body)
 		json_response = boi[0]["article"]
-		# binding.pry
+		binding.pry
 
-
-		discussion = Discussion.create(name: json_response["headline"].split(" ").join("_"), group: Group.find(params[:group_id]), article_url: params[:article_url])
-		article = Article.create(title: json_response["headline"], content: json_response["articleBodyHtml"], discussion: discussion)
-		# # if article.valid?
-		
-		# # binding.pry
-		render json: DiscussionSerializer.new(discussion).to_serialized_json
+		if json_response
+			discussion = Discussion.create(name: json_response["headline"].split(" ").join("_"), group: Group.find(params[:group_id]), article_url: params[:article_url])
+			article = Article.create(title: json_response["headline"], content: json_response["articleBodyHtml"], discussion: discussion)
+			render json: DiscussionSerializer.new(discussion).to_serialized_json			
+		end
 	end
 
 	def show
